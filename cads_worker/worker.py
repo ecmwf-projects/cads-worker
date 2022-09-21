2024e5879a6c399e38207de46fa6dc109c60bdaf
francesconazzaro <francesconazzaro89@gmail.com>
2022-09-21 10:39:17 +0200

    move cacholote import and cache_key compute

--- cads_worker/worker.py
+++ cads_worker/worker.py
@@ -3,8 +3,6 @@ import logging
 import os
 from typing import Any
 
-import cacholote
-
 
 def submit_workflow(
     setup_code: str,
@@ -12,6 +10,7 @@ def submit_workflow(
     kwargs: dict[str, Any] = {},
     metadata: dict[str, Any] = {},
 ) -> str:
+    import cacholote
 
     exec(setup_code, globals())
     logging.info(f"Submitting: {metadata['process_id']}")
@@ -26,10 +25,10 @@ def submit_workflow(
         ),
         io_delete_original=True,
     ):
-        func(metadata=metadata, **kwargs)
         cache_key = cacholote.hexdigestify_python_call(
             func, metadata=metadata, **kwargs
         )
+        func(metadata=metadata, **kwargs)
         cache_dict = json.loads(cacholote.config.SETTINGS["cache_store"][cache_key])
     public_dict = {
         k: {} if k.endswith(":storage_options") else v for k, v in cache_dict.items()
