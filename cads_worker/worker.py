cb8db2410ca7d0cef66558fbeeb7639a1d0009ca
malmans2 <mattia.almansi@jhu.edu>
2022-09-21 07:53:59 +0200

    get json from cache store

--- cads_worker/worker.py
+++ cads_worker/worker.py
@@ -3,27 +3,35 @@ import logging
 import os
 from typing import Any
 
+import cacholote
+
 
 def submit_workflow(
     setup_code: str,
     entry_point: str,
     kwargs: dict[str, Any] = {},
     metadata: dict[str, Any] = {},
-) -> dict[str, Any] | list[dict[str, Any]]:
-    import cacholote
+) -> str:
 
-    cacholote.config.set(
-        cache_files_urlpath=os.path.join(f"s3://{os.environ['CACHE_BUCKET']}"),
+    exec(setup_code, globals())
+    logging.info(f"Submitting: {metadata['process_id']}")
+    func = eval(entry_point)
+    with cacholote.config.set(
+        cache_files_urlpath=f"s3://{os.environ['CACHE_BUCKET']}",
+        cache_files_urlpath_readonly=f"{os.environ['STORAGE_API_URL']}/{os.environ['CACHE_BUCKET']}",
         cache_files_storage_options=dict(
             key=os.environ["STORAGE_ADMIN"],
             secret=os.environ["STORAGE_PASSWORD"],
             client_kwargs={"endpoint_url": os.environ["OBJECT_STORAGE_URL"]},
         ),
-    )
-    exec(setup_code, globals())
-    logging.info(f"Submitting: {metadata['process_id']}")
-    results = eval(f"{entry_point}(metadata=metadata, **kwargs)")
-    results = json.loads(cacholote.encode.dumps(results))
-    results["href"] = f"{os.environ['STORAGE_API_URL']}/{results['file:local_path']}"
-    results["xarray:storage_options"] = {}
-    return json.dumps(results)
+        io_delete_original=True,
+    ):
+        func(metadata=metadata, **kwargs)
+        cache_key = cacholote.hexdigestify_python_call(
+            func, metadata=metadata, **kwargs
+        )
+        cache_dict = json.loads(cacholote.config.SETTINGS["cache_store"][cache_key])
+    public_dict = {
+        k: {} if k.endswith(":storage_options") else v for k, v in cache_dict.items()
+    }
+    return json.dumps(public_dict)
