15da00f92bf38ab164cc6896e3586a11d9898c83
Mattia Almansi <m.almansi@bopen.eu>
2022-10-12 09:37:07 +0200

    move cacholote import and refactor global settings

--- cads_worker/worker.py
+++ cads_worker/worker.py
@@ -1,29 +1,22 @@
+import contextlib
 import json
 import logging
 import os
 import shutil
 import tempfile
 import time
-from typing import Any
+from typing import Any, Iterator
 
 logging.basicConfig(level=logging.INFO)
 
 
-def submit_workflow(
-    setup_code: str,
-    entry_point: str,
-    kwargs: dict[str, Any] = {},
-    metadata: dict[str, Any] = {},
-) -> str:
+try:
     import cacholote
-
-    exec(setup_code, globals())
-    logging.info(f"Submitting: {kwargs}")
-    # cache key is computed from function name and kwargs, we add 'setup_code' to kwargs so functions
-    # with the same name and with different setup_code have different caches
-    kwargs.setdefault("config", {})["__setup_code__"] = setup_code
-    func = eval(entry_point)
-    with cacholote.config.set(
+except ImportError:
+    # cacholote is not installed on the broker
+    pass
+else:
+    cacholote.config.set(
         cache_files_urlpath=f"s3://{os.environ['CACHE_BUCKET']}",
         cache_files_urlpath_readonly=f"{os.environ['STORAGE_API_URL']}/{os.environ['CACHE_BUCKET']}",
         cache_files_storage_options=dict(
@@ -32,21 +25,43 @@ def submit_workflow(
             client_kwargs={"endpoint_url": os.environ["OBJECT_STORAGE_URL"]},
         ),
         io_delete_original=True,
-    ):
-        cache_key = cacholote.hexdigestify_python_call(
-            func, metadata=metadata, **kwargs
-        )
-        results_dir = os.path.join(tempfile.gettempdir(), cache_key)
-        # wait for the running process that is writing in the results_dir
-        while os.path.exists(results_dir):
-            time.sleep(2)
-        os.mkdir(results_dir)
-        os.chdir(results_dir)
+    )
+
+
+@contextlib.contextmanager
+def change_working_directory(path: str) -> Iterator[None]:
+    origin = os.getcwd()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(origin)
+
+
+def submit_workflow(
+    setup_code: str,
+    entry_point: str,
+    kwargs: dict[str, Any] = {},
+    metadata: dict[str, Any] = {},
+) -> str:
+
+    exec(setup_code, globals())
+    logging.info(f"Submitting: {kwargs}")
+    # cache key is computed from function name and kwargs, we add 'setup_code' to kwargs so functions
+    # with the same name and with different setup_code have different caches
+    kwargs.setdefault("config", {})["__setup_code__"] = setup_code
+    func = eval(entry_point)
+    cache_key = cacholote.hexdigestify_python_call(func, metadata=metadata, **kwargs)
+    results_dir = os.path.join(tempfile.gettempdir(), cache_key)
+    # wait for the running process that is writing in the results_dir
+    while os.path.exists(results_dir):
+        time.sleep(2)
+    with change_working_directory(results_dir):
         try:
             func(metadata=metadata, **kwargs)
         finally:
             shutil.rmtree(results_dir)
-        cache_dict = json.loads(cacholote.config.SETTINGS["cache_store"][cache_key])
+    cache_dict = json.loads(cacholote.config.SETTINGS["cache_store"][cache_key])
     public_dict = {
         k: {} if k.endswith(":storage_options") else v for k, v in cache_dict.items()
     }
