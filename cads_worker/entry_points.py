8bc57f2ad223d9be4810f02cbe562aeb0e02e491
francesconazzaro <francesconazzaro89@gmail.com>
2022-10-24 11:19:49 +0200

    add cache cleaner entry point

---
+++ cads_worker/entry_points.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+import cacholote
+import typer
+
+
+app = typer.Typer()
+
+
+@app.command()
+def cache_cleaner():
+    with cacholote.config.set(
+        cache_files_urlpath=f"s3://{os.environ['CACHE_BUCKET']}",
+        cache_files_urlpath_readonly=f"{os.environ['STORAGE_API_URL']}/{os.environ['CACHE_BUCKET']}",
+        cache_files_storage_options=dict(
+            key=os.environ["STORAGE_ADMIN"],
+            secret=os.environ["STORAGE_PASSWORD"],
+            client_kwargs={"endpoint_url": os.environ["OBJECT_STORAGE_URL"]},
+            asynchronous=False,
+        ),
+        io_delete_original=True,
+        raise_all_encoding_errors=True,
+        cache_db_urlpath=f"postgresql://{os.environ['COMPUTE_DB_USER']}"
+        f":{os.environ['COMPUTE_DB_PASSWORD']}@{os.environ['COMPUTE_DB_HOST']}"
+        f"/{os.environ['COMPUTE_DB_NAME']}",
+    ):
+        while True:
+            print(os.getcwd())
+            cacholote.cleaner.clean_cache_files(
+                max_size=os.environ.get("MAX_SIZE", 200_000_000),
+                method=os.environ.get("METHOD", "LRU"),
+            )
+            time.sleep(1)
