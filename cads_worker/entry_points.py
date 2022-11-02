e2c9b26fa701828041dd7449f03dde63b582322e
francesconazzaro <francesconazzaro89@gmail.com>
2022-11-02 13:56:43 +0000

    update cache cleaner implementation

--- cads_worker/entry_points.py
+++ cads_worker/entry_points.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import time
 
 import cacholote
@@ -13,6 +14,8 @@ def cache_cleaner():
             secret=os.environ["STORAGE_PASSWORD"],
             client_kwargs={"endpoint_url": os.environ["OBJECT_STORAGE_URL"]},
             asynchronous=False,
+            use_listings_cache=False,
+            skip_instance_cache=False,
         ),
         raise_all_encoding_errors=True,
         cache_db_urlpath=f"postgresql://{os.environ['COMPUTE_DB_USER']}"
@@ -20,12 +23,14 @@ def cache_cleaner():
         f"/{os.environ['COMPUTE_DB_USER']}",
     ):
         while True:
-            print(os.getcwd())
-            cacholote.clean_cache_files(
-                maxsize=os.environ.get("MAX_SIZE", 200_000_000),
-                method=os.environ.get("METHOD", "LRU"),
-            )
-            time.sleep(1)
+            try:
+                cacholote.clean_cache_files(
+                    maxsize=os.environ.get("MAX_SIZE", 200_000_000),
+                    method=os.environ.get("METHOD", "LRU"),
+                )
+            except:
+                logging.exception('cache_cleaner crashed')
+            time.sleep(0.1)
 
 
 def main():
