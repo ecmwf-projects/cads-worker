d33e592bef33a4ada7dfd3b58874247808d0b421
francesconazzaro <francesconazzaro89@gmail.com>
2022-11-15 16:50:35 +0100

    drop the infinite loop. the cache cleaner is now a cronjob

--- cads_worker/entry_points.py
+++ cads_worker/entry_points.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import time
+from datetime import datetime
 
 import cacholote
 import typer
@@ -22,25 +23,16 @@ def cache_cleaner() -> None:
         f":{os.environ['COMPUTE_DB_PASSWORD']}@{os.environ['COMPUTE_DB_HOST']}"
         f"/{os.environ['COMPUTE_DB_USER']}",
     ):
-        counter = 0
-        frequency_delete_unknown_files = 10
-        while True:
-            counter += 1
-            delete_unknown_files = not counter % frequency_delete_unknown_files
-            if delete_unknown_files:
-                logging.info(f"cache_cleaner {delete_unknown_files=}")
-                counter = 0
-
-            try:
-                cacholote.clean_cache_files(
-                    maxsize=os.environ.get("MAX_SIZE", 200_000_000),
-                    method=os.environ.get("METHOD", "LRU"),
-                    delete_unknown_files=delete_unknown_files,
-                )
-            except Exception as ex:
-                logging.exception(f"cache_cleaner crashed: {ex!r}")
-
-            time.sleep(0.1)
+        logging.warning(
+            f"Running cache cleaner {datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')}"
+        )
+        try:
+            cacholote.clean_cache_files(
+                maxsize=os.environ.get("MAX_SIZE", 200_000_000),
+                method=os.environ.get("METHOD", "LRU"),
+            )
+        except Exception as ex:
+            logging.exception(f"cache_cleaner crashed: {ex!r}")
 
 
 def main() -> None:
