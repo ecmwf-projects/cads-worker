5c0955eeef3ee2945c0a7acb70ea6f45d96af194
francesconazzaro <francesconazzaro89@gmail.com>
2022-12-22 15:23:03 +0100

    set default for cache max size

--- cads_worker/entry_points.py
+++ cads_worker/entry_points.py
@@ -25,7 +25,7 @@ def _cache_cleaner() -> None:
         logging.warning("Running cache cleaner: %s", datetime.datetime.now())
         try:
             cacholote.clean_cache_files(
-                maxsize=int(os.environ.get("MAX_SIZE", 200_000_000)),
+                maxsize=int(os.environ.get("MAX_SIZE", 1_000_000_000)),
                 method=os.environ.get("METHOD", "LRU"),  # type: ignore[arg-type] # let cacholote handle it
             )
         except Exception:
