53d01de1ca0044265262c1350614db19017ee851
Mattia Almansi <m.almansi@bopen.eu>
2023-01-23 16:48:23 +0100

    use env variables to set cacholote

--- cads_worker/worker.py
+++ cads_worker/worker.py
@@ -21,31 +21,14 @@ def submit_workflow(
     # with the same name and with different setup_code have different caches
     kwargs.setdefault("config", {})["__setup_code__"] = setup_code
     func = eval(entry_point)
-    with cacholote.config.set(
-        cache_files_urlpath=f"s3://{os.environ['CACHE_BUCKET']}",
-        cache_files_urlpath_readonly=(
-            f"{os.environ['STORAGE_API_URL'].rstrip('/')}/{os.environ['CACHE_BUCKET']}"
-        ),
-        cache_files_storage_options=dict(
-            key=os.environ["STORAGE_ADMIN"],
-            secret=os.environ["STORAGE_PASSWORD"],
-            client_kwargs={"endpoint_url": os.environ["OBJECT_STORAGE_URL"]},
-            asynchronous=False,
-        ),
-        io_delete_original=True,
-        raise_all_encoding_errors=True,
-        cache_db_urlpath=f"postgresql://{os.environ['COMPUTE_DB_USER']}"
-        f":{os.environ['COMPUTE_DB_PASSWORD']}@{os.environ['COMPUTE_DB_HOST']}"
-        f"/{os.environ['COMPUTE_DB_USER']}",
-    ):
-        cwd = os.getcwd()
-        with tempfile.TemporaryDirectory() as tmpdir:
-            os.chdir(tmpdir)
-            try:
-                func(
-                    metadata=metadata, **kwargs, __context__=contextvars.copy_context()
-                )
-            finally:
-                os.chdir(cwd)
+    cwd = os.getcwd()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        os.chdir(tmpdir)
+        try:
+            func(
+                metadata=metadata, **kwargs, __context__=contextvars.copy_context()
+            )
+        finally:
+            os.chdir(cwd)
 
     return cacholote.cache.LAST_PRIMARY_KEYS.get()
