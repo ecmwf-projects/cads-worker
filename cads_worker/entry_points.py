71650cef28ac7c0bd8742ad52c5e9905734ff7fa
Francesco Nazzaro <francesconazzaro89@gmail.com>
2023-03-14 10:03:58 +0100

    add delete_unknown_files configuration

--- cads_worker/entry_points.py
+++ cads_worker/entry_points.py
@@ -17,6 +17,8 @@ def _cache_cleaner() -> None:
             maxsize=int(os.environ.get("MAX_SIZE", 1_000_000_000)),
             method=os.environ.get("METHOD", "LRU"),  # type: ignore[arg-type] # let cacholote handle it
             logger=LOGGER,
+            delete_unknown_files=bool(os.environ.get("DELETE_UNKNOWN_FILES", 0)),
+            lock_validity_period=float(os.environ.get("LOCK_VALIDITY_PERIOD", 60 * 60 * 24))
         )
     except Exception:
         LOGGER.exception("cache_cleaner crashed")
