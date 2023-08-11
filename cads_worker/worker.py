e2ca430b26491c87805c984a481092f4b8b06ca0
Francesco Nazzaro <francesconazzaro89@gmail.com>
2023-08-11 17:18:55 +0200

    catch logs and send them to dask

--- cads_worker/worker.py
+++ cads_worker/worker.py
@@ -2,9 +2,10 @@ import os
 import tempfile
 from typing import Any
 
-import cacholote
 import distributed.worker
 import structlog
+
+import cacholote
 
 from . import config
 
@@ -29,15 +30,20 @@ def submit_workflow(
     LOGGER.info("Processing job", job_id=job_id)
     adaptor_class = cads_adaptors.get_adaptor_class(entry_point, setup_code)
     adaptor = adaptor_class(form=form, **config)
+    context = cads_adaptors.adaptor.Context()
     cwd = os.getcwd()
     with tempfile.TemporaryDirectory() as tmpdir:
         os.chdir(tmpdir)
         try:
-            result = cacholote.cacheable(adaptor.retrieve)(request=request)
+            result = cacholote.cacheable(adaptor.retrieve)(request=request, context=context)
         except Exception:
+            distributed.worker.get_worker().log_event(topic=job_id, msg=context.stdout)
+            LOGGER.info(context.stdout, job_id=job_id)
             LOGGER.exception(job_id=job_id)
             raise
         finally:
             os.chdir(cwd)
 
+    distributed.worker.get_worker().log_event(topic=job_id, msg=context.stdout)
+    LOGGER.info(context.stdout, job_id=job_id)
     return result.id  # type: ignore
