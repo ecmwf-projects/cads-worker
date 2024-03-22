63c10457052900d560717dd46fb9c50f7340fa84
Mattia Almansi <m.almansi@bopen.eu>
2024-03-22 16:28:19 +0100

    restore prev cleaner + raise

--- cads_worker/entry_points.py
+++ cads_worker/entry_points.py
@@ -12,18 +12,21 @@ cacholote.config.set(logger=LOGGER)
 
 
 def _cache_cleaner() -> None:
-    cache_bucket = os.environ.get("CACHE_BUCKET", None)
     max_size = int(os.environ.get("MAX_SIZE", 1_000_000_000))
-    method = os.environ.get("METHOD", "LRU")
-    delete_unknown_files = bool(os.environ.get("DELETE_UNKNOWN_FILES", 1))
-    lock_validity_period = float(os.environ.get("LOCK_VALIDITY_PERIOD", 60 * 60 * 24))
+    cache_bucket = os.environ.get("CACHE_BUCKET", None)
     LOGGER.info("Running cache cleaner", max_size=max_size, cache_bucket=cache_bucket)
-    cacholote.clean_cache_files(
-        maxsize=max_size,
-        method=method,
-        delete_unknown_files=delete_unknown_files,
-        lock_validity_period=lock_validity_period,
-    )
+    try:
+        cacholote.clean_cache_files(
+            maxsize=max_size,
+            method=os.environ.get("METHOD", "LRU"),  # type: ignore[arg-type] # let cacholote handle it
+            delete_unknown_files=bool(os.environ.get("DELETE_UNKNOWN_FILES", 1)),
+            lock_validity_period=float(
+                os.environ.get("LOCK_VALIDITY_PERIOD", 60 * 60 * 24)
+            ),
+        )
+    except Exception:
+        LOGGER.exception("cache_cleaner crashed")
+        raise
 
 
 def cache_cleaner() -> None:
