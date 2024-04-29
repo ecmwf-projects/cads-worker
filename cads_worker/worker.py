7ab78c8118cd441e28175ef1fce8a99d481d9037
Francesco Nazzaro <francesconazzaro89@gmail.com>
2024-04-29 10:04:49 +0200

    add file-like logger

--- cads_worker/worker.py
+++ cads_worker/worker.py
@@ -36,9 +36,24 @@ def ensure_session(func):
 
 
 class Context:
-    def __init__(self, job_id: str, logger: Any):
+    def __init__(self, job_id: str, logger: Any, write_type: str = "stdout"):
         self.job_id = job_id
         self.logger = logger
+        self.write_type = write_type
+        self.messages_buffer = ""
+
+    def write(self, message: str) -> None:
+        """Use the logger as a file-like object. Needed by tqdm progress bar."""
+        self.messages_buffer += message
+
+    def flush(self) -> None:
+        """Write to the logger the content of the buffer."""
+        if self.messages_buffer:
+            if self.write_type == "stdout":
+                self.add_stdout(self.messages_buffer)
+            elif self.write_type == "stderr":
+                self.add_stderr(self.messages_buffer)
+            self.messages_buffer = ""
 
     @ensure_session
     def add_user_visible_log(self, message: str, session: Any = None) -> None:
