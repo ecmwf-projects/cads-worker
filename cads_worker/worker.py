3e390c29d1ed12239762cac1aad1fa819095f579
Francesco Nazzaro <francesconazzaro89@gmail.com>
2024-05-31 12:53:22 +0200

    add user_visible_errors

--- cads_worker/worker.py
+++ cads_worker/worker.py
@@ -202,8 +202,10 @@ def submit_workflow(
         try:
             request = {k: request[k] for k in sorted(request.keys())}
             result = cacholote.cacheable(adaptor.retrieve)(request=request)
-        except Exception:
+        except Exception as err:
             logger.exception(job_id=job_id, event_type="EXCEPTION")
+            context.add_user_visible_error(f"The job failed with: {err.__class__.__name__}")
+            context.error(f"{err.__class__.__name__}: {str(err)}")
             raise
         finally:
             os.chdir(cwd)
