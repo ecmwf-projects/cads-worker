915b6f520fa541247d35de002af37f6c430a7898
Francesco Nazzaro <francesconazzaro89@gmail.com>
2024-07-24 17:17:11 +0200

    chmod only with s3

--- cads_worker/worker.py
+++ cads_worker/worker.py
@@ -219,7 +219,8 @@ def submit_workflow(
         finally:
             os.chdir(cwd)
     fs, _ = cacholote.utils.get_cache_files_fs_dirname()
-    fs.chmod(result.result["args"][0]["file:local_path"], acl="public-read")
+    if (local_path := result.result["args"][0]["file:local_path"]).startswith("s3://"):
+        fs.chmod(local_path, acl="public-read")
     with context.session_maker() as session:
         request = cads_broker.database.set_request_cache_id(
             request_uid=job_id,
