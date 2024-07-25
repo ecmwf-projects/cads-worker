fed3358c838816ecdee2fd253a59c1f1d9a66527
Mattia Almansi <m.almansi@bopen.eu>
2024-07-25 12:23:09 +0200

    improve expiration args

--- tests/test_20_expire_cache_entries.py
+++ tests/test_20_expire_cache_entries.py
@@ -1,14 +1,14 @@
+import contextlib
 import datetime
-import pathlib
+from pathlib import Path
+from typing import Any
 
 import cacholote
 import pytest
 
 from cads_worker import entry_points
 
-TODAY = datetime.datetime.now(tz=datetime.timezone.utc)
-TOMORROW = TODAY + datetime.timedelta(days=1)
-YESTERDAY = TODAY - datetime.timedelta(days=1)
+does_not_raise = contextlib.nullcontext
 
 
 @cacholote.cacheable
@@ -17,20 +17,24 @@ def cached_now() -> datetime.datetime:
 
 
 @pytest.mark.parametrize(
-    "collection_id,before,after",
+    "collection_id,all_collections,raises",
     [
-        (["foo"], None, None),
-        (None, TOMORROW, None),
-        (None, None, YESTERDAY),
-        (["foo"], TOMORROW, YESTERDAY),
+        (["foo"], False, does_not_raise()),
+        ([], True, does_not_raise()),
+        ([], False, pytest.raises(ValueError)),
+        (["foo"], True, pytest.raises(ValueError)),
     ],
 )
 def test_cache_entries(
-    tmp_path: pathlib.Path,
-    collection_id: list[str] | None,
-    before: datetime.datetime | None,
-    after: datetime.datetime | None,
+    tmp_path: Path,
+    collection_id: list[str],
+    all_collections: bool,
+    raises: contextlib.nullcontext[Any],
 ) -> None:
+    today = datetime.datetime.now(tz=datetime.timezone.utc)
+    tomorrow = today + datetime.timedelta(days=1)
+    yeasterday = today - datetime.timedelta(days=1)
+
     with cacholote.config.set(
         cache_db_urlpath=f"sqlite:///{tmp_path / 'cacholote.db'}",
         tag="foo",
@@ -38,10 +42,12 @@ def test_cache_entries(
         now = cached_now()
         assert now == cached_now()
 
-        count = entry_points._expire_cache_entries(
-            collection_id=collection_id,
-            before=before,
-            after=after,
-        )
-        assert count == 1
-        assert now != cached_now()
+        with raises:
+            count = entry_points._expire_cache_entries(
+                before=tomorrow,
+                after=yeasterday,
+                collection_id=collection_id,
+                all_collections=all_collections,
+            )
+            assert count == 1
+            assert now != cached_now()
