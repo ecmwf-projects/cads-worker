cbe7d9814104e1165a99b11c18f40fd62dffa8fd
Mattia Almansi <m.almansi@bopen.eu>
2024-07-31 17:23:28 +0200

    expand env vars

--- cads_worker/utils.py
+++ cads_worker/utils.py
@@ -6,4 +6,4 @@ def parse_data_volumes_config(path: str | None = None) -> list[str]:
         path = os.environ["DATA_VOLUMES_CONFIG"]
 
     with open(path) as fp:
-        return fp.read().splitlines()
+        return [os.path.expandvars(line.rstrip("\n")) for line in fp]
--- tests/test_30_utils.py
+++ tests/test_30_utils.py
@@ -9,8 +9,10 @@ def test_utils_parse_data_volumes_config(
     tmp_path: pathlib.Path,
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
+    monkeypatch.setenv("FOO", "foo")
+    monkeypatch.setenv("BAR", "bar")
     data_volumes_config = tmp_path / "data-volumes.config"
-    data_volumes_config.write_text("foo\nbar")
+    data_volumes_config.write_text("$FOO\n${BAR}")
     assert utils.parse_data_volumes_config(str(data_volumes_config)) == ["foo", "bar"]
 
     monkeypatch.setenv("DATA_VOLUMES_CONFIG", str(data_volumes_config))
