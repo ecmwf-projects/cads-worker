5e4d573b776e8edeb3220707fdceb2398f6a6d70
malmans2 <m.almansi@bopen.eu>
2025-02-05 16:37:17 +0100

    cleanup

--- cads_worker/entry_points.py
+++ cads_worker/entry_points.py
@@ -74,12 +74,17 @@ def _expire_cache_entries(
         datetime.datetime,
         Option(help="Expire entries created after this date"),
     ],
-    collection_id: Annotated[list[str], Option(help="Collection ID to expire")] = [],
+    collection_id: Annotated[
+        list[str],
+        Option(help="Collection ID to expire"),
+    ] = [],
     all_collections: Annotated[
-        bool, Option("--all-collections", help="Expire all collections")
+        bool,
+        Option(help="Expire all collections"),
     ] = False,
     delete: Annotated[
-        bool, Option(help="Delete entries to expire")
+        bool,
+        Option(help="Delete entries to expire"),
     ] = False,
     batch_size: Annotated[
         int | None,
