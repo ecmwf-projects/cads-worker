5564cb428fc7cbce718e900106bc7aa112c9a033
malmans2 <m.almansi@bopen.eu>
2026-04-07 12:38:16 +0200

    Allow setting cache max size using strings

--- tests/test_01_models.py
+++ tests/test_01_models.py
@@ -9,15 +9,15 @@ def test_data_volumes_from_yaml(
     tmp_path: pathlib.Path,
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
-    monkeypatch.setenv("MAX_SIZE", "10")
+    monkeypatch.setenv("MAX_SIZE", "10MiB")
     data_volumes_config = tmp_path / "data-volumes.yaml"
-    data_volumes_config.write_text("\nfoo:\nbar:\n  weight: 0\n  max_size: 20\n")
+    data_volumes_config.write_text("\nfoo:\nbar:\n  weight: 0\n  max_size: 10Mb\n")
 
     volumes = DataVolumes.from_yaml(str(data_volumes_config))
     assert volumes.model_dump() == {
         "volumes": {
-            "foo": {"weight": 1, "max_size": 10},
-            "bar": {"weight": 0, "max_size": 20},
+            "foo": {"weight": 1, "max_size": 10_485_760},
+            "bar": {"weight": 0, "max_size": 10_000_000},
         }
     }
 
