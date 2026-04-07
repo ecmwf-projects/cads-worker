300436bacb339ddf65855317b5409f777131c49b
malmans2 <m.almansi@bopen.eu>
2026-04-07 12:42:30 +0200

    better testing

--- tests/test_01_models.py
+++ tests/test_01_models.py
@@ -11,13 +11,16 @@ def test_data_volumes_from_yaml(
 ) -> None:
     monkeypatch.setenv("MAX_SIZE", "10MiB")
     data_volumes_config = tmp_path / "data-volumes.yaml"
-    data_volumes_config.write_text("\nfoo:\nbar:\n  weight: 0\n  max_size: 10Mb\n")
+    data_volumes_config.write_text(
+        "\nfoo:\nbar:\n  weight: 0\n  max_size: 10Mb\nbaz:\n  weight: 0\n  max_size: 10\n"
+    )
 
     volumes = DataVolumes.from_yaml(str(data_volumes_config))
     assert volumes.model_dump() == {
         "volumes": {
             "foo": {"weight": 1, "max_size": 10_485_760},
             "bar": {"weight": 0, "max_size": 10_000_000},
+            "baz": {"weight": 0, "max_size": 10},
         }
     }
 
