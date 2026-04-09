324525b535793bd5f6018ec149b5036b16575368
Francesco Nazzaro <francesconazzaro89@gmail.com>
2026-04-09 16:03:53 +0200

    Add volume filtering (#73)
    * Add volume filtering
    
    * qa
    
    * qa
    
    * Add logging for unavailable data volumes in filter_available_volumes method
    
    * fix root_path
    
    * generalise for all local filesystems
    
    ---------
    
    Co-authored-by: malmans2 <m.almansi@bopen.eu>

--- cads_worker/models.py
+++ cads_worker/models.py
@@ -1,9 +1,14 @@
 import os
 import random
+import urllib
 from typing import Self
 
+import fsspec
+import structlog
 import yaml
 from pydantic import BaseModel, Field, NonNegativeFloat, NonNegativeInt
+
+LOGGER = structlog.get_logger(__name__)
 
 
 def get_env_max_size() -> int:
@@ -18,10 +23,26 @@ class DataVolumeConfig(BaseModel):
 class DataVolumes(BaseModel):
     volumes: dict[str, DataVolumeConfig]
 
+    def filter_available_volumes(self) -> dict[str, DataVolumeConfig]:
+        available_volumes = {}
+        for volume in self.volumes:
+            parsed = urllib.parse.urlparse(volume)
+            fs = fsspec.filesystem(parsed.scheme)
+            if (
+                isinstance(fs, fsspec.implementations.local.LocalFileSystem)
+                and parsed.path.startswith("/")
+                and not os.path.ismount(f"/{parsed.path.split('/')[1]}")
+            ):
+                LOGGER.warning(f"Volume {volume} is not available. Skipping it.")
+                continue
+            available_volumes[volume] = self.volumes[volume]
+        return available_volumes
+
     def get_random_volume(self) -> str:
+        available_volumes = self.filter_available_volumes()
         (volume,) = random.choices(
-            list(self.volumes),
-            weights=[config.weight for config in self.volumes.values()],
+            list(available_volumes),
+            weights=[config.weight for config in available_volumes.values()],
             k=1,
         )
         return volume
--- tests/test_01_models.py
+++ tests/test_01_models.py
@@ -11,14 +11,30 @@ def test_data_volumes_from_yaml(
 ) -> None:
     monkeypatch.setenv("MAX_SIZE", "10")
     data_volumes_config = tmp_path / "data-volumes.yaml"
-    data_volumes_config.write_text("\nfoo:\nbar:\n  weight: 0\n  max_size: 20\n")
+    data_volumes_config.write_text(
+        "s3://foo:\ns3://bar:\n  weight: 0\n  max_size: 20\n"
+    )
 
     volumes = DataVolumes.from_yaml(str(data_volumes_config))
     assert volumes.model_dump() == {
         "volumes": {
-            "foo": {"weight": 1, "max_size": 10},
-            "bar": {"weight": 0, "max_size": 20},
+            "s3://foo": {"weight": 1, "max_size": 10},
+            "s3://bar": {"weight": 0, "max_size": 20},
         }
     }
 
-    assert volumes.get_random_volume() == "foo"
+    assert volumes.get_random_volume() == "s3://foo"
+
+    data_volumes_config.write_text(
+        "s3://foo:\n  weight: 1\ncci1:///bar:\n  weight: 100_000\n"
+    )
+
+    volumes = DataVolumes.from_yaml(str(data_volumes_config))
+    assert volumes.model_dump() == {
+        "volumes": {
+            "s3://foo": {"weight": 1, "max_size": 10},
+            "cci1:///bar": {"weight": 100_000, "max_size": 10},
+        }
+    }
+
+    assert volumes.get_random_volume() == "s3://foo"
